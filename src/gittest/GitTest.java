cab1ffb6f7eefbe0211fc8d65d646a60bb6d719f
jwx <15353714844@163.com>
2018-03-15 18:08:58 +0800

    测试git

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -3,8 +3,5 @@ package gittest;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("hello git");
-		System.out.println("commit");
-		System.out.println("this is in branchtest");
-		System.out.println("git test again");
 	}
 }
