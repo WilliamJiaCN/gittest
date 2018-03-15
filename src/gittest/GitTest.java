1edccb755cbcdf396a5a714e2579761c22fdef12
WilliamJiaCN <wenxiong.jia@gmail.com>
2018-03-15 17:54:48 +0800

    Update GitTest.java

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
 		System.out.println("hello git");
 		System.out.println("commit");
 		System.out.println("this is in branchtest");
+		System.out.println("git test again");
 	}
 }
