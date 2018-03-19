7417f80fa15c05e58384629cb7b5b561baf20347
jwx <15353714844@163.com>
2018-03-19 10:48:00 +0800

    git test again

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("git branch dev");
 		System.out.println("git push test");
+		System.out.println("git push test again");
 	}
 }
